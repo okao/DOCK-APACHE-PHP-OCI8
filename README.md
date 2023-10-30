97e809aff7b7c9c9c9f73c00a6e58a2c2878fe5a
Hamzath Anees <hamzath.anees@mib.com.mv>
2023-10-30 16:30:51 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VERSION 0.1
+## Please update this repo when needed
---
+++ www/test/index.php
@@ -0,0 +1,17 @@
+<?php
+  
+  //sum of two numbers
+  function sum($a, $b) {
+    return $a + $b;
+  }
+
+  //difference of two numbers
+  function difference($a, $b) {
+    return $a - $b;
+  }
+
+  //show the result of sum and difference
+  echo "Sum: " . sum(5, 10) . "<br>";
+
+  echo "Difference: " . difference(5, 10) . "<br>";
+?>
